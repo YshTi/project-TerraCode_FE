c33b5ce4e6a33937d37e67158547287eb697efc6
MaksOrlenko <144942642+MaksOrlenko@users.noreply.github.com>
2026-07-09 01:05:54 +0300

    Add TravellerCard component

---
+++ src/components/traveller-card/traveller-card.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+
+import { ButtonLink } from "@/components/buttons/button";
+
+import styles from "./traveller-card.module.css";
+
+type TravellerCardProps = {
+  _id: string;
+  name: string;
+  avatarUrl?: string;
+  articlesAmount: number;
+};
+
+export function TravellerCard({
+  _id,
+  name,
+  avatarUrl,
+  articlesAmount,
+}: TravellerCardProps) {
+  return (
+    <article className={styles.card}>
+      <Image
+        src={avatarUrl || "/default-avatar.png"}
+        alt={`${name} avatar`}
+        width={130}
+        height={130}
+        className={styles.photo}
+      />
+
+      <div className={styles.infoWrapper}>
+        <h3 className={styles.name}>{name}</h3>
+
+        <p className={styles.articlesCount}>
+          Статей: {articlesAmount}
+        </p>
+      </div>
+
+      <ButtonLink
+        href={`/traveller/${_id}`}
+        variant="secondary"
+        className={styles.linkButton}
+      >
+        Переглянути профіль
+      </ButtonLink>
+    </article>
+  );
+}
