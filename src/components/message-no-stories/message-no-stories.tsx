beb1f4ecaef1dfb027056c61ff644956e172690a
Larimar4you <Zajanezlaja@gmail.com>
2026-07-08 13:06:57 +0200

    Add MessageNoStories component

---
+++ src/components/message-no-stories/message-no-stories.tsx
@@ -0,0 +1,25 @@
+import { ButtonLink } from "@/components/button/button";
+
+import styles from "./message-no-stories.module.css";
+
+type MessageNoStoriesProps = {
+  text: string;
+  buttonText: string;
+  linkTo: string;
+};
+
+export function MessageNoStories({
+  text,
+  buttonText,
+  linkTo,
+}: MessageNoStoriesProps) {
+  return (
+    <div className={styles.wrapper}>
+      <p className={styles.text}>{text}</p>
+
+      <ButtonLink href={linkTo} className={styles.button}>
+        {buttonText}
+      </ButtonLink>
+    </div>
+  );
+}
