7f79338f1afedf17657020c110ff0e06f956bf7a
v.sementsov <v.sementsov@andersenlab.com>
2026-07-09 20:30:19 +0200

    feat: add home page

---
+++ src/components/about/about.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@/components/container/container";
+import styles from "./about.module.css";
+
+export function About() {
+  return (
+    <section className="section">
+      <Container>
+        <h2>Мандруй екологічно та відкривай нові горизонти</h2>
+      </Container>
+    </section>
+  );
+}
