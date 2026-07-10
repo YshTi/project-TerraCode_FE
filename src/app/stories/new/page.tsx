46a7cdd15d7920177733c286f4f2c573dae8fe35
grytsaidmitry <dimasicus2008@gmail.com>
2026-07-10 23:04:19 +0200

    Add files via upload

--- src/app/stories/new/page.tsx
+++ src/app/stories/new/page.tsx
@@ -1,3 +1,34 @@
-export default function Page() {
-  return <div>Page is under construction</div>;
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { Container } from "@/components/container/container";
+import { PageTitle } from "@/components/page-title/page-title";
+import { AddStoryForm } from "@/components/add-story-form/add-story-form";
+import { Loader } from "@/components/loader/loader";
+import { useAuth } from "@/providers/auth-provider";
+
+export default function AddStoryPage() {
+  const router = useRouter();
+  const { user, isLoading } = useAuth();
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      router.replace("/auth/login");
+    }
+  }, [isLoading, user, router]);
+
+  if (isLoading || !user) {
+    return <Loader />;
+  }
+
+  return (
+    <section className="section">
+      <Container>
+        <PageTitle>Створити нову історію</PageTitle>
+        <AddStoryForm />
+      </Container>
+    </section>
+  );
 }
