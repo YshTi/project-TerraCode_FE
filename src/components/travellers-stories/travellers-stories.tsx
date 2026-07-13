bc43b0c33cb90c674e97f8353d8c74925f3ff3ab
YushTi <zebra2368@gmail.com>
2026-07-13 18:36:29 +0200

    traveller-stories module mockup

--- src/components/travellers-stories/travellers-stories.tsx
+++ src/components/travellers-stories/travellers-stories.tsx
@@ -2,6 +2,8 @@
 
 import { useMemo } from "react";
 
+import { Loader } from "@/components/loader/loader";
+import { Pagination } from "@/components/pagination/pagination";
 import StoryCard from "@/components/story-card/story-card";
 import type { Story } from "@/types/story";
 
@@ -10,12 +12,18 @@ import css from "./travellers-stories.module.css";
 interface TravellersStoriesProps {
   stories: Story[];
   savedStoryIds?: string[];
+  hasMore: boolean;
+  isLoadingMore: boolean;
+  onLoadMore: () => void;
   className?: string;
 }
 
 export function TravellersStories({
   stories,
   savedStoryIds = [],
+  hasMore,
+  isLoadingMore,
+  onLoadMore,
   className = "",
 }: TravellersStoriesProps) {
   const savedIds = useMemo(
@@ -24,14 +32,29 @@ export function TravellersStories({
   );
 
   return (
-    <ul className={`${css.list} ${className}`}>
-      {stories.map((story) => (
-        <StoryCard
-          key={story._id}
-          story={story}
-          isSaved={savedIds.has(story._id)}
+    <div className={`${css.wrapper} ${className}`}>
+      <ul className={css.list}>
+        {stories.map((story) => (
+          <StoryCard
+            key={story._id}
+            story={story}
+            isSaved={savedIds.has(story._id)}
+          />
+        ))}
+      </ul>
+
+      {isLoadingMore ? (
+        <div className={css.loaderWrapper}>
+          <Loader />
+        </div>
+      ) : (
+        <Pagination
+          onLoadMore={onLoadMore}
+          isLoading={isLoadingMore}
+          hasMore={hasMore}
+          fullWidthOnMobile
         />
-      ))}
-    </ul>
+      )}
+    </div>
   );
 }
