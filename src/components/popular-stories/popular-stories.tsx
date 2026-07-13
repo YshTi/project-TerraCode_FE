00adb844530394f7f8bc0eddf8165201c826b400
Tymofii Danilov <timadanilov2002@gmail.com>
2026-07-13 21:33:49 +0200

    popular stories section done

--- src/components/popular-stories/popular-stories.tsx
+++ src/components/popular-stories/popular-stories.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Container } from "@/components/container/container";
 import css from "./popular-stories.module.css";
-import { Navigation, Pagination } from "swiper/modules";
+import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
@@ -11,6 +11,8 @@ import { Loader } from "../loader/loader";
 import StoryCard from "../story-card/story-card";
 import { useAuth } from "@/providers/auth-provider";
 import { useMemo } from "react";
+import { Button, ButtonLink } from "../buttons/button";
+import { SpriteIcon } from "../sprite-icon/sprite-icon";
 
 export function PopularStories() {
   const { user } = useAuth();
@@ -46,33 +48,71 @@ export function PopularStories() {
   return (
     <section className="section">
       <Container className={css.wrapper}>
-        <h2 className={css.heading}>Популярні статті</h2>
+        <div className={css.headingBtnWrap}>
+          <h2 className={css.heading}>Популярні статті</h2>
+          <ButtonLink href="/stories" className={css.allArticlesBtnPc}>
+            Всі статті
+          </ButtonLink>
+        </div>
         {isLoading && (
           <div className={css.loaderWrapper}>
             <Loader />
           </div>
         )}
         {!isLoading && isSuccess && data.stories.length > 0 && (
-          <Swiper
-            modules={[Navigation]}
-            slidesPerView={3}
-            spaceBetween={24}
-            navigation
-            pagination={{
-              clickable: true,
-            }}
-            className="mySwiper"
-          >
-            {data.stories.map((story) => (
-              <SwiperSlide key={story._id}>
-                <StoryCard
-                  key={story._id}
-                  story={story}
-                  isSaved={savedIds.has(story._id)}
-                />
-              </SwiperSlide>
-            ))}
-          </Swiper>
+          <>
+            <div className={css.swiperWrapper}>
+              <Swiper
+                modules={[Navigation]}
+                breakpoints={{
+                  320: {
+                    slidesPerView: 1,
+                  },
+                  768: {
+                    slidesPerView: 2,
+                  },
+                  1440: {
+                    slidesPerView: 3,
+                  },
+                }}
+                spaceBetween={24}
+                navigation={{
+                  nextEl: ".swiper-button-next-custom",
+                  prevEl: ".swiper-button-prev-custom",
+                }}
+                pagination={{
+                  clickable: true,
+                }}
+                className="mySwiper"
+              >
+                {data.stories.map((story) => (
+                  <SwiperSlide key={story._id}>
+                    <StoryCard
+                      story={story}
+                      isSaved={savedIds.has(story._id)}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+            <div className={css.navButtons}>
+              <Button
+                variant="secondary"
+                className={`${css.navBtn} swiper-button-prev-custom`}
+              >
+                <SpriteIcon id="icon-arrow_back" />
+              </Button>
+              <Button
+                variant="secondary"
+                className={`${css.navBtn} swiper-button-next-custom`}
+              >
+                <SpriteIcon id="icon-arrow_forward" />
+              </Button>
+            </div>
+            <ButtonLink href="/stories" className={css.allArticlesBtnMob}>
+              Всі статті
+            </ButtonLink>
+          </>
         )}
       </Container>
     </section>
