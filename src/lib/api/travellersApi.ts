0072d35575ed56ea9d908c43315f448329d26b71
YushTi <zebra2368@gmail.com>
2026-07-13 18:32:50 +0200

    fixed authProvider

--- src/lib/api/travellersApi.ts
+++ src/lib/api/travellersApi.ts
@@ -1,43 +1,43 @@
-// import { nextServer } from "./api";
+import { nextServer } from "./api";
 
-// import { User } from "@/types/users";
+import { User } from "@/types/users";
 
-// interface UsersResponse {
-//   status: number;
-//   data: User[];
-//   pagination: {
-//     page: number;
-//     limit: number;
-//     total: number;
-//     totalPages: number;
-//   };
-// }
+interface UsersResponse {
+  status: number;
+  data: User[];
+  pagination: {
+    page: number;
+    limit: number;
+    total: number;
+    totalPages: number;
+  };
+}
 
-// interface UserResponse {
-//   status: number;
-//   data: User;
-// }
+interface UserResponse {
+  status: number;
+  data: User;
+}
 
-// export const getTravellers = async (
-//   page = 1,
-//   limit = 12,
-// ): Promise<UsersResponse> => {
-//   const response = await nextServer.get<UsersResponse>("/users", {
-//     params: {
-//       page,
-//       limit,
-//     },
-//   });
+export const getTravellers = async (
+  page = 1,
+  limit = 12,
+): Promise<UsersResponse> => {
+  const response = await nextServer.get<UsersResponse>("/users", {
+    params: {
+      page,
+      limit,
+    },
+  });
 
-//   return response.data;
-// };
+  return response.data;
+};
 
-// export const getTravellerById = async (
-//   userId: string,
-// ): Promise<User> => {
-//   const response = await nextServer.get<UserResponse>(
-//     `/users/${userId}`,
-//   );
+export const getTravellerById = async (
+  userId: string,
+): Promise<User> => {
+  const response = await nextServer.get<UserResponse>(
+    `/users/${userId}`,
+  );
 
-//   return response.data.data;
-// };
+  return response.data.data;
+};
